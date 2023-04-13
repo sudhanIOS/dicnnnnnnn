94f38d2e661708a17af6bd27c8cf1bbf9722467b
Hariharasudhan <hari@Hariharasudhans-MacBook-Pro.local>
2023-04-13 19:00:34 +0530

    ADDED

---
+++ dic/SecondObj.swift
@@ -0,0 +1,13 @@
+//
+//  SecondObj.swift
+//  dic
+//
+//  Created by Hariharasudhan on 13/04/23.
+//  Copyright © 2023 Indium. All rights reserved.
+//
+
+import UIKit
+
+class SecondObj: NSObject {
+
+}
